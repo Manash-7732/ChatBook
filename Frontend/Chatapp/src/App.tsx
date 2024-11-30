f54c66718c28d3d2ccb6303cf30ba99842fad505
Manash-7732 <manashraj2222@gmail.com>
2024-11-30 15:11:24 +0530

    UI-DONE

---
+++ Frontend/Chatapp/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import Home from './_ui_design/pages/Home'
+import SignUp from './_ui_design/pages/SignUp'
+import Login from './_ui_design/pages/Login'
+function App() {
+ 
+
+  return (
+    <>
+       <div className='p-4 h-screen flex items-center justify-center'>
+         <Routes>
+          <Route path='/' element = {<Home/>} />
+          <Route path='/signup' element = {<SignUp/>} />
+          <Route path='/login' element = {<Login/>} />
+         </Routes>
+       </div>
+
+    </>
+  )
+}
+
+export default App
